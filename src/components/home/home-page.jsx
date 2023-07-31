8fb15f14c09b749e678cd49394a2ca817a692d2f
Ben Seebeck <bas112@duke.edu>
2023-07-31 16:54:19 -0400

    style updates

--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -6,7 +6,12 @@ const HomePage = ({ data }) => (
     {data?.map((ev) => (
       <Link className="card" key={ev.id} href={`/events/${ev.id}`}>
         <div className="image">
-          <Image fill={true} alt={ev.title} src={ev.image} />
+          <Image
+            fill={true}
+            // sizes="(max-width:250px)"
+            alt={ev.title}
+            src={ev.image}
+          />
         </div>
         <div className="content">
           <h2>{ev.title}</h2> <p>{ev.description}</p>
